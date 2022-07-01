46fdb950bacfabad65d2baa0ee03362ba75fc339
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2022-07-01 10:42:25 +0200

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [ user, setUser ] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const checkLocalStorage = async () => {
+    const userString = await localStorage.getItem("user");    //returns the user details
+    const user = JSON.parse(userString);    //converts the user details to object
+
+    if(user) {    
+      console.log(`Welcome back ${user.userName}`);
+    }
+  }
+  
+  //When component mounts check if local storage has details
+  useEffect(() => {
+    checkLocalStorage();
+  }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();   //prevents the browser from refreshing
+    setUser({...user, [e.target.id] : e.target.value});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>How To Persist A Logged-In User In React</h1>
+      <input type="text" id="userName" placeholder="Username" value={user.userName} onChange={handleChange}/>
+      <input type="text" id="password" placeholder="Password" value={user.password} onChange={handleChange}/> 
+      <button>Login</button>
+    </form>
   );
 }
 
 export default App;
+
